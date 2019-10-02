316548650f1d70da95ec02e4f9596036a5321697
bnu <bnufactory@gmail.com>
2019-10-02 19:15:40 +0900

    새 UI 적용

--- views/login.blade.php
+++ views/login.blade.php
@@ -1,16 +1,23 @@
 @inject('plugin', 'Xpressengine\Plugins\SocialLogin\Plugin')
 {{ XeFrontend::css($plugin->asset('assets/auth.css'))->load() }}
-<!--소셜로그인-->
+
 <div class="user __xe_userLogin">
-    <div class="auth-sns v2">
-        <h1>{{xe_trans('xe::doLogin')}}</h1>
-        <ul>
+    <div class="auth-sns-user">
+        <h2 class="user__title">{{ xe_trans('xe::login') }}</h2>
+        <ul class="auth-sns-user-list">
             @foreach($providers as $provider => $info)
-                <li class="sns-{{ $provider }}"><a href="{{ route('social_login::auth', ['provider' => $provider]) }}"><i class="xi-{{ $provider }}"></i>{{ xe_trans('social_login::signInBy', ['provider' => xe_trans($info['title'])]) }}</a></li>
+            <li class="auth-sns-user__item--{{ $provider }}">
+                <a href="{{ route('social_login::auth', ['provider' => $provider]) }}">
+                    {{ xe_trans('social_login::signInBy', ['provider' => xe_trans($info['title'])]) }}
+                </a>
+            </li>
             @endforeach
-            <li class="sns-email"><a href="{{ route('login', ['by' => 'email']) }}"><i class="xi-mail-o"></i>{{ xe_trans('social_login::signInBy', ['provider' => xe_trans('xe::email')]) }}</a></li>
+            <li class="auth-sns-user__item--email">
+                <a href="{{ route('login', ['by' => 'email']) }}">
+                    {{ xe_trans('social_login::signInBy', ['provider' => xe_trans('xe::email')]) }}
+                </a>
+            </li>
         </ul>
-        <p class="auth-text"><a href="{{ route('auth.register') }}">{{xe_trans('xe::doSignUp')}}</a></p>
+        <p class="auth-user__text"><a href="{{ route('auth.register') }}">{{ xe_trans('xe::doSignUp') }}</a></p>
     </div>
 </div>
-<!--//소셜로그인-->
